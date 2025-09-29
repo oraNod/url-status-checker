27d88cab9d2c64a6e1b231c7c4cd39fdf2a7479b
Don Naro <dnaro@redhat.com>
2025-09-29 13:55:51 +0100

    test against RTD domain

--- url_checker.py
+++ url_checker.py
@@ -14,7 +14,7 @@ from urllib.parse import urljoin
 
 import requests
 
-HOST: str = "http://docs.testing.ansible.com"
+HOST: str = "https://ansible.readthedocs.io/"
 REQUEST_TIMEOUT: int = 30  # seconds
 
 
